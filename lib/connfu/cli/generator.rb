6702bc0a8c0c96cb02c3709c40e3a4316659bacc
juandebravo <juandebravo@gmail.com>
2011-09-28 10:46:10 +0100

    FEATURE added channels filter to CLI scaffold

--- lib/connfu/cli/generator.rb
+++ lib/connfu/cli/generator.rb
@@ -1,42 +1,38 @@
+require 'connfu/listener_channel'
+
 module Connfu
 	##
 	# This module helps a client to manage connFu applications
   module Cli
     module Generator
       
-      APPLICATION_TEMPLATE=<<END
+      VOICE_CHANNEL=<<END
 
-require 'connfu'
-
-##
-# This application is an example of how to create a connFu application
-
-token = "YOUR-VALID-CONNFU-TOKEN"
-
-Connfu.logger = STDOUT
-Connfu.log_level = Logger::DEBUG
-
-Connfu.application(token) {
-
-    listen(:voice) do |conference|
+      listen(:voice) do |conference|
         conference.on(:join) do |call|
-                puts "New inbound call from \#{call[:from]} on number \#{call[:to]}"
-            end
+          puts "New inbound call from \#{call[:from]} on number \#{call[:to]}"
+        end
 
-            conference.on(:leave) do |call|
-                puts "\#{call[:from]} has left the conference \#{call[:channel_name]}"
-            end
+        conference.on(:leave) do |call|
+          puts "\#{call[:from]} has left the conference \#{call[:channel_name]}"
+        end
 
-            conference.on(:new_topic) do |topic|
-                puts "New topic in the conference \#{topic[:channel_name]}: \#{topic[:content]}"
-            end
+        conference.on(:new_topic) do |topic|
+          puts "New topic in the conference \#{topic[:channel_name]}: \#{topic[:content]}"
+        end
       end
 
+END
+      TWITTER_CHANNEL=<<END
+      
       listen(:twitter) do |twitter|
             twitter.on(:new) do |tweet|
                 puts "\#{tweet[:channel_name]} just posted a new tweet in the conference room: \#{tweet.content}"
             end
       end
+      
+END
+      SMS_CHANNEL=<<END
 
       listen(:sms) do |sms|
             sms.on(:new) do |message|
@@ -44,7 +40,33 @@ Connfu.application(token) {
             end
       end
 
-}
+END
+
+      RSS_CHANNEL=<<END
+
+      listen(:rss) do |rss|
+            rss.on(:new) do |post|
+                puts "New post with title \#{post[:channel_name]} in the blog \#{post[:bare_uri]}"
+            end
+      end
+
+END
+
+      APPLICATION_TEMPLATE=<<END
+
+  require 'connfu'
+
+  ##
+  # This application is an example of how to create a connFu application
+
+  token = "YOUR-VALID-CONNFU-TOKEN"
+
+  Connfu.logger = STDOUT
+  Connfu.log_level = Logger::DEBUG
+
+  Connfu.application(token) {
+  %{channels}
+  }
 
 END
       
@@ -58,10 +80,32 @@ END
         #
         # ==== Return
         def run(name, channels = nil, file_name = "application.rb")
+
+          channels.is_a?(String) and channels = channels.split.map{|channel| channel.to_sym}
+          
+          channels_templates = {:voice => VOICE_CHANNEL, :sms => SMS_CHANNEL, :twitter => TWITTER_CHANNEL, :rss => RSS_CHANNEL}
+
+          code = APPLICATION_TEMPLATE.dup
+
+          if channels.nil?
+            channels = channels_templates.values.join
+          else
+            channels.delete_if{|channel| !Connfu::ListenerChannel::CHANNEL_TYPES.include?(channel)}
+            channels = channels.map{|item| channels_templates[item]}.join
+          end
+          values = {:channels => channels}
+          
+          code.gsub!(/%\{(\w+)\}/) do |match|
+            key = $1
+            values[key.to_sym]
+          end
+          puts code
+          exit
+          
           Dir.mkdir(name)
             Dir.chdir(name) do
             File.open(file_name, 'w') do |f|
-              f.write(APPLICATION_TEMPLATE)
+              f.write(code)
             end
           end
         end # end:run
